0473fc5f830aa94f9b09ce80585d22b9550e7845
AdwaithPA <54847975+AdwaithPA@users.noreply.github.com>
2021-06-06 21:38:15 +0530

    Add files via upload

---
+++ src/components/APP.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./HEADER";
+import Footer from "./FOOTER";
+import BODY from "./BODY";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <BODY />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BODY.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div className="note">
+      <h1>REACT.JS SHORT INTRO</h1>
+      <p>
+      React is a declarative, efficient, and flexible JavaScript library for building user interfaces. ReactJS is an open-source, component-based front end library responsible only for the view layer of the application.
+      </p>
+    </div>
+  );
+}
+
+export default Body;
